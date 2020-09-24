9d773468791ea014d8ef3f4b645b5e1222377b05
carlosandres-mendez <carlosandresucr@gmail.com>
2020-09-24 13:26:43 -0600

    Ejemplo para probar Java

---
+++ Ejemplo1.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author carlosandres.mendez
+ */
+public class Ejemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo!");
+  
+    }
+}
